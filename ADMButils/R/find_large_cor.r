f74a1df2ebd9c4d09b7da65273e0333f7450522f
Jeff Laake <jefflaake@gmail.com>
2012-10-19 14:03:12 -0700

    Bug patch

--- ADMButils/R/find_large_cor.r
+++ ADMButils/R/find_large_cor.r
@@ -11,11 +11,11 @@
 #' in which matrix element satisfies logical expression
 #' @export
 #' @author Jeff Laake
-find_large_cor=function(x,rho)
+find_large_cor=function(x,rho=0.9)
 {
     if(!is.matrix(x))stop("x must be a matrix\n")
 	if(nrow(x)!=ncol(x))stop("x must be square\n")
-	if(any(x[upper.tri(x)]!=x[lower.tri(x)]))stop("x must be symmetric\n")
+	if(any(t(x)[lower.tri(t(x))]!=x[lower.tri(x)]))stop("x must be symmetric\n")
 	if(any(abs(x)>1.00001))stop("x does not appear to be a correlation matrix\n")
 	if(is.null(rownames(x)))rownames(x)=1:nrow(x)
 	if(is.null(colnames(x)))colnames(x)=1:ncol(x)
